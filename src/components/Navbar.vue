3217c118864796a64909e57f6af3e5caf22ea718
dakioso <daniel.milojevic@student.kyh.se>
2019-04-03 13:15:21 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-toolbar flat dark>
+        <v-toolbar-title></v-toolbar-title>
+        <span>Digitalist</span>
+        <span class="font-weight-light">Events</span>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat to="/">
+          Hem
+      </v-btn>
+      <v-btn flat to="/about">
+        Events
+      </v-btn>
+    </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+
+
+<script>
+export default {
+    
+}
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p class="red">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum praesentium, quis sequi dignissimos dolore minus aut mollitia, nulla suscipit fugiat earum doloribus sint dolor repudiandae! Sapiente iste dolores culpa maxime!</p>
+    <blockquote class="blockquote">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</blockquote>
   </div>
 </template>
